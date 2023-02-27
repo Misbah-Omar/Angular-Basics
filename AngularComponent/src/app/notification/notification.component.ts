bb9043e3f11162943c63e6ed1e5bf0a8f0171847
Misbah Omar <misbah.omar@planckepoch.com>
2023-02-27 16:12:46 +0530

    componets

---
+++ AngularComponent/src/app/notification/notification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  template: `<div>
+              <p>This website uses cookies for better user experience.</p>
+            </div>`,
+  styleUrls: ['./notification.component.css']
+})
+export class NotificationComponent {
+
+}
