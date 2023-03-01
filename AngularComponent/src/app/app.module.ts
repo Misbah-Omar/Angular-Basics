cc4037000949e015efeb56160d5d557e8b70584f
Misbah Omar <misbah.omar@planckepoch.com>
2023-03-01 09:52:35 +0530

    implemented two way binding for search box using ngModel

--- AngularComponent/src/app/app.module.ts
+++ AngularComponent/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './Container/container.component';
 import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
 import { NotificationComponent } from './notification/notification.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
@@ -14,10 +16,12 @@ import { NotificationComponent } from './notification/notification.component';
     ContainerComponent,
     NavComponent,
     HeaderComponent,
-    NotificationComponent
+    NotificationComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularComponent/src/app/notification/notification.component.ts
+++ AngularComponent/src/app/notification/notification.component.ts
@@ -3,10 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notification',
   template: `<div class="alert alert-success" [hidden]="displayNotification">
-              <p>This website uses cookies for better user experience.</p>
+              This website uses cookies for better user experience.
+              <div class="close"><button class="btn" (click)="closeNotification()">X</button></div>
             </div>`,
-  styles: [".notification-div{margin: 10px 0px; padding: 10px 20px; backgrounf-color: #FAD7A0; text-align: center;}", "p{font-size: 14px;}"]
+  styles: [".notification-div{margin: 10px 0px; padding: 10px 20px; backgrounf-color: #FAD7A0; text-align: center;}", "p{font-size: 14px;}",".close{float: right; margin-top: -15px;}"]
 })
 export class NotificationComponent {
   displayNotification: boolean = false;
+
+  closeNotification(){
+    this.displayNotification = true;
+  }
 }
